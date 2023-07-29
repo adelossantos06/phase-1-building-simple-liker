933bc753404237344ed40307e3e6d909603b64f1
Alyssa De Los Santos <adelossantos6@gmail.com>
2023-07-29 16:38:31 -0500

    completes simple liker app lab

--- main.js
+++ main.js
@@ -2,7 +2,36 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
 // Your JavaScript code goes here!
+
+
+
+
+let likeBtns = document.querySelectorAll(".like")
+
+
+for (e of likeBtns) {
+  e.addEventListener("click", (e) => {
+    const heart = e.target
+    mimicServerCall()
+      .then(res => {
+        heart.innerHTML = FULL_HEART
+        heart.className = "activated-heart"
+      })
+      .then("click", revert => {
+        heart.innerHTML = EMPTY_HEART
+        heart.className = "like-glyph"
+      })
+      .catch((error) => {
+        document.querySelector("#modal").className = ""
+        setTimeout(function () {
+          document.querySelector("#modal".className = "hidden", 3000)
+        })
+      })
+
+  })
+}
 
 
 
@@ -11,9 +40,9 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
